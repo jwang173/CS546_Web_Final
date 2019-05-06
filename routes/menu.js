beddcf66b69b1da1818d62722486aa06ddaaf8c3
jwang173 <49701313+jwang173@users.noreply.github.com>
2019-05-07 03:36:23 +0800

    Update menu.js
    New routes/menu

--- routes/menu.js
+++ routes/menu.js
@@ -6,10 +6,31 @@ const {
 
 // GET todos/
 router.get('/', async (req, res) => {
-  res.json({
-    "status": "yet to implement menu slash"
-  });
+  try{
+    let foodList = await menu.getAll();
+    res.status(200).json(foodList);
+  } catch(e) {
+    res.status(404).json({error:e});
+  }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    let food = await menu.getById(req.params.id);
+    res.status(200).json(food);
+  } catch(e) {
+    res.status(404).json({error:e});
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    let name = req.body.search;
+    let result = await menu.getByName(name);
+    res.status(200).json(result);
+  } catch(e) {
+    res.status(404).json({error:e});
+  }
+})
 
 module.exports = router;
