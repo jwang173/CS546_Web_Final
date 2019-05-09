daff016bf7d790b63a8bca7147171af209b26a7d
MattSorrentino <msorren2@stevens.edu>
2019-05-09 13:48:43 -0400

    [Matt] made edits to try to get the app to run

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const app = express();
+
 const morgan = require('morgan'); // small library for our logger
 const routes = require('./routes');
 const exphbs = require("express-handlebars");
 
-const app = express();
+
+const static = express.static(__dirname + "/public");
+app.use("/", static);
 
 // Middlewares Here
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(morgan('dev')); // helper for logging our routes to the console
 
 app.engine("handlebars", exphbs({
   defaultLayout: "main"
 }));
 app.set("view engine", "handlebars");
-
-
 
 // Add API routes
 routes(app);
@@ -28,3 +28,36 @@ routes(app);
 app.listen(3000, () => {
   console.log('Server Listening on localhost:3000');
 });
+
+
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const app = express();
+// const static = express.static(__dirname + "/public");
+// // const cookie = require('cookie-parser');
+// const session = require('express-session')
+// const routes = require("./routes");
+// const exphbs = require("express-handlebars");
+
+// app.use("/", static);
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// // app.use(cookie());
+
+// app.use(session({
+//     name: 'AuthCookie',
+//     secret: 'some secret string!',
+//     resave: false,
+//     saveUninitialized: true
+// }))
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+// routes(app);
+
+// app.listen(3000, () => {
+//     console.log("We've now got a server!");
+//     console.log("Your routes will be running on http://localhost:3000");
+// });
