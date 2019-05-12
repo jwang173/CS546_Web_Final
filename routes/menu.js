5f8a997b3f695565535847e680255d669957c450
jwang173 <49701313+jwang173@users.noreply.github.com>
2019-05-12 12:35:41 +0800

    Update menu.js in routes
    I have changed image showing in routes.

--- routes/menu.js
+++ routes/menu.js
@@ -1,36 +1,58 @@
 const router = require('express').Router();
 
 const {
-  menu
+  menu,
+  upload
 } = require('../data');
 
 // GET todos/
 router.get('/', async (req, res) => {
   try{
-    let foodList = await menu.getAll();
-    res.status(200).json(foodList);
+    let foodList = await upload.getAll();
+    foodList.forEach(element => {
+      let arrExt = element.mMimetype;
+      if(arrExt == "image/png" || arrExt == "image/jpeg" || arrExt == "image/bmp") {
+        element.img = true;
+      }
+    })
+    // console.log(foodList);
+    res.render("menu/menu", {food: foodList, css: "some.css"})
+    // res.status(200).json(foodList);
   } catch(e) {
-    res.status(404).json({error:e});
+    res.render("menu/menu", {errMsg: e});
   }
 });
 
-router.get('/:id', async (req, res) => {
-  try {
-    let food = await menu.getById(req.params.id);
-    res.status(200).json(food);
-  } catch(e) {
-    res.status(404).json({error:e});
-  }
-})
+// router.get('/:id', async (req, res) => {
+//   try {
+//     let food = await menu.getById(req.params.id);
+//     res.status(200).json(food);
+//   } catch(e) {
+//     res.status(404).json({error:e});
+//   }
+// })
 
 router.post('/', async (req, res) => {
   try {
     let name = req.body.search;
     let result = await menu.getByName(name);
-    res.status(200).json(result);
+    let List = result;
+    let Liststr = JSON.stringify(result);
+    List.forEach(element => {
+      let arrExt = element.mMimetype;
+      if(arrExt == "image/png" || arrExt == "image/jpeg" || arrExt == "image/bmp") {
+        element.img = true;
+      }
+    });
+    if(Liststr == JSON.stringify([])) {
+      res.render("menu/menu", {errMsg: "Menu not found"});
+    } else {
+      res.render("menu/menu", {food: List, css: "some.css"})
+    }
   } catch(e) {
-    res.status(404).json({error:e});
+    res.render("menu/menu", {errMsg: e});
   }
 })
 
 module.exports = router;
+
